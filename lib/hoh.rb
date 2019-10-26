49ba7a82a6b70f34705be98614807c6f6bdec366
Yining Wang <yining.wang@utexas.edu>
2019-10-26 01:48:55 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,9 +1,9 @@
-# Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-# that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
-#
+# # Build a nested HoH of the classification of biological life on Earth based
+# # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
+# # that HoH as the return value for the method "naming_system."
+# #
+# # The tests will guide your construction.
+# #
 # PORTION_1 = {
 #   label: "Kingdom",
 #   sub_category: {
@@ -13,18 +13,18 @@
 #     }
 #   }
 # }
-# 
+# # 
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+# # 
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+# # 
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
@@ -41,5 +41,25 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-
+  bio = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class",
+      sub_category: {
+        label: "Order",
+        sub_category: {
+          label: "Family",
+          sub_category: {
+            label: "Genus",
+            sub_category: {
+              label: "Species"
+            }
+          }
+        }
+      }
+    }
+  }
+}
 end
